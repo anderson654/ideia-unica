b71ba2017587d76f84786f035a484ef60070d61e
anderson654 <andersong.salvador@gmail.com>
2021-02-16 16:46:56 -0300

    Teste1

--- pages/api/moedas.js
+++ pages/api/moedas.js
@@ -2,9 +2,9 @@ async function moedas(request,response){
     const apiSecret = process.env.CONVERTKIT_API_SECRET;
     const dynamicDate = new Date();
 
-    const moneyResponse = await fetch(`https://fxmarketapi.com/apilive?api_key=${apiSecret}&currency=EURUSD`);
+    const moneyResponse = await fetch(`https://www.freeforexapi.com/api/live?pairs=${apiSecret}`);
     const moneyResponseJson = await moneyResponse.json();
-    const moneyValor = moneyResponseJson.price;
+    const moneyValor = moneyResponseJson.rates;
     //controle de atualização em cache não colocar em informaçoes sensiveis
     response.setHeader('Cache-Control','s-maxage=10 , stale-while-revalidate');
 
