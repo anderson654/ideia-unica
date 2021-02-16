86d199ca66ce8113416c64c845e926b87ac08564
anderson654 <andersong.salvador@gmail.com>
2021-02-16 15:34:08 -0300

    Teste1

--- pages/api/moedas.js
+++ pages/api/moedas.js
@@ -5,6 +5,8 @@ async function moedas(request,response){
     const moneyResponse = await fetch(`https://www.freeforexapi.com/api/live?pairs=${apiSecret}`);
     const moneyResponseJson = await moneyResponse.json();
     const moneyValor = moneyResponseJson.rates;
+    //controle de atualização em cache não colocar em informaçoes sensiveis
+    response.setHeader('Cache-Control','s-maxage=10 , stale-while-revalidate');
 
     response.json({
         date:dynamicDate.toGMTString(),
