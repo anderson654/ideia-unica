82bac89cea33ee2ddb0f7fddc27a24b927871edd
anderson654 <andersong.salvador@gmail.com>
2021-02-16 15:23:37 -0300

    Teste1

--- pages/api/moedas.js
+++ pages/api/moedas.js
@@ -1,7 +1,14 @@
-function moedas(request,response){
+async function moedas(request,response){
+    const apiSecret = process.env.CONVERTKIT_API_SECRET;
     const dynamicDate = new Date();
+
+    const moneyResponse = await fetch(`https://www.freeforexapi.com/api/live?pairs=${apiSecret}`);
+    const moneyResponseJson = await moneyResponse.json();
+    const moneyValor = moneyResponseJson.rates;
+
     response.json({
-        date:dynamicDate.toGMTString()
+        date:dynamicDate.toGMTString(),
+        money: moneyValor
     });
 }
 export default moedas;
